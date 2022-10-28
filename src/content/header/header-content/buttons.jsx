7a64c1eec7bfae487a9ee1ed4e5a8c619e613a15
josé vitor <josevitorpinheiro2002@gmail.com>
2022-10-28 15:28:19 -0300

    new

--- src/content/header/header-content/buttons.jsx
+++ src/content/header/header-content/buttons.jsx
@@ -7,16 +7,16 @@ import "./buttons.css";
 export default function Buttons() {
   return (
     <div className="buttons-container">
-      <Link to="/" className="Link">
+      <Link to="/TerraMap-Site/" className="Link">
         Home
       </Link>
-      <Link to="/serviços" className="Link">
+      <Link to="/TerraMap-Site/serviços" className="Link">
         Serviços
       </Link>
-      <Link to="/sobre" className="Link">
+      <Link to="/TerraMap-Site/sobre" className="Link">
         Sobre
       </Link>
-      <Link to="/contato" className="Link">
+      <Link to="/TerraMap-Site/contato" className="Link">
         Contato
       </Link>
     </div>
